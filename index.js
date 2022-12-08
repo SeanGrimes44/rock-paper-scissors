14fcf5eca23e68cb4813498df79aa55eb8b0a84a
Sean <73760350+SeanGrimes44@users.noreply.github.com>
2022-12-07 21:33:52 -0500

    Play a round of rock paper scissors

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function getComputerChoice () {
     let number = Math.floor(Math.random() * 3);
     if (number === 0) {
@@ -10,3 +11,32 @@ function getComputerChoice () {
         return "scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "Tie game!";
+    }
+    else if (playerSelection === "rock" && computerSelection == "paper") {
+        return "You lose! Paper beats Rock";
+    }
+    else if (playerSelection === "rock" && computerSelection == "scissors") {
+        return "You win! Rock beats Scissors";
+    }
+    else if (playerSelection === "paper" && computerSelection == "rock") {
+        return "You Win! Paper beats Rock";
+    }
+    else if (playerSelection === "paper" && computerSelection == "scissors") {
+        return "You lose! Scissors beats Paper";
+    }
+    else if (playerSelection === "scissors" && computerSelection == "paper") {
+        return "You Win! Scissors beats Paper";
+    }
+    else if (playerSelection === "scissors" && computerSelection == "rock") {
+        return "You lose! Rock beats Scissors";
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
