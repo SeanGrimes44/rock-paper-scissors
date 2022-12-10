cb244f99d86325cc57ecbf242ed5dd33f59a2410
Sean <73760350+SeanGrimes44@users.noreply.github.com>
2022-12-10 11:15:04 -0500

    Display the score of each game

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
 const display = document.querySelector("#display");
+const scoreDisplay = document.querySelector("#score-display");
+
+let playerScore = 0;
+let computerScore = 0;
 
 rock.addEventListener("click", () => {
     playRound("rock", getComputerChoice());
@@ -38,26 +42,38 @@ function playRound(playerSelection, computerSelection) {
     }
     else if (playerSelection === "rock" && computerSelection == "paper") {
         display.textContent = "You lose! Paper beats Rock";
+        computerScore++;
+        scoreDisplay.textContent = playerScore + " to " + computerScore;
         return -1;
     }
     else if (playerSelection === "rock" && computerSelection == "scissors") {
         display.textContent = "You win! Rock beats Scissors";
+        playerScore++;
+        scoreDisplay.textContent = playerScore + " to " + computerScore;
         return 1;
     }
     else if (playerSelection === "paper" && computerSelection == "rock") {
         display.textContent = "You Win! Paper beats Rock";
+        playerScore++;
+        scoreDisplay.textContent = playerScore + " to " + computerScore;
         return 1;
     }
     else if (playerSelection === "paper" && computerSelection == "scissors") {
         display.textContent = "You lose! Scissors beats Paper";
+        computerScore++;
+        scoreDisplay.textContent = playerScore + " to " + computerScore;
         return -1;
     }
     else if (playerSelection === "scissors" && computerSelection == "paper") {
         display.textContent = "You Win! Scissors beats Paper";
+        playerScore++;
+        scoreDisplay.textContent = playerScore + " to " + computerScore;
         return 1;
     }
     else if (playerSelection === "scissors" && computerSelection == "rock") {
         display.textContent = "You lose! Rock beats Scissors";
+        computerScore++;
+        scoreDisplay.textContent = playerScore + " to " + computerScore;
         return -1;
     }
 }
